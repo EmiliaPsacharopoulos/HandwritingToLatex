b6099b4ec81f232a7d5d5e0b1f490f564c6344f7
jdwishart <103145543+jdwishart@users.noreply.github.com>
2022-04-07 14:57:10 -0400

    Changes to edge script
    Now will go all the way from image to Latex, classifying not great though

--- matlabHandwriting/edgesd.m
+++ matlabHandwriting/edgesd.m
@@ -2,10 +2,11 @@
 % (https://www.mathworks.com/matlabcentral/fileexchange/46812-two-dimensional-gaussian-hi-pass-and-low-pass-image-filter), 
 % MATLAB Central File Exchange. Retrieved March 30, 2022. 
 
-cd('/Users/jamesdavid/Documents/Winter2022/EECS 351/project/babyProject/');
-X = load('Matlab/trainset.mat');
-Y = load('Matlab/trainsetLabels.mat');
-Mdl = fitctree(X.images,Y.labels); %works
+cd('/Users/jamesdavid/Documents/Winter2022/EECS 351/project/matlabHandwriting/');
+Xo = load('Matlab/trainset.mat');
+Yo = load('Matlab/trainsetLabels.mat');
+% Mdl = fitctree(Xo.images,Yo.labels); %works
+Mdl = fitcknn(Xo.images,Yo.labels,'NumNeighbors',5,'Standardize',1);
 close all;
 pic = im2double(imread('IMG_6782.jpg'));
 pic = im2gray(pic);
@@ -92,21 +93,21 @@ alreadyDone = zeros(length(B),1);
 aDidex = 1;
 endCol = size(L,2);
 col = 1;
+outputString = "";
 while(col < endCol)
     col = col + 1;
-    colSum = sum(text(:,col-1));
-    if(colSum < 1000)
+    colSum = sum(text(:,col));
+    if(colSum < 750)
         continue;
     end
-    
     maxLets = 0;
-    maxCol = col-1;
-    minC = col - 1 - 20;
-    maxC = col - 1 + 20;
-    if(col - 1 - 20 <= 0)
+    maxCol = col;
+    minC = col - 0;
+    maxC = col + 50;
+    if(col - 0 <= 0)
         minC = 1;
     end
-    if(col - 1 + 20 > size(L,2))
+    if(col + 20 > size(L,2))
         maxC = size(L,2);
     end
     for coln = minC:maxC
@@ -118,15 +119,19 @@ while(col < endCol)
             maxCol = coln;
         end
     end
-
-
+    
+    firstLet = 1;
+    prevRow = size(L,1);
     for row = size(L,1):-1:1
 
         if(L(row,maxCol) ~= 0 && ~ismember(L(row,maxCol),alreadyDone))
-            
+            if(firstLet)
+                outputString = append(outputString," \\newline ");
+            end
             bound = B{L(row,maxCol)};
             letter = poly2mask(bound(:,2),bound(:,1),size(pic,1),size(pic,2));
             picNew = pic .* letter;
+            picNew(picNew == 0) = 0.75;
             imshow(picNew);
 %             letter = ~letter;
 %             imshow(letter);
@@ -136,10 +141,131 @@ while(col < endCol)
             set(gca,'view',[90 -90]);
             alreadyDone(aDidex) = L(row,maxCol);
             aDidex = aDidex + 1;
+            saveas(gcf,'/Users/jamesdavid/Documents/Winter2022/EECS 351/project/matlabHandwriting/tempFolder/current.png');
+            let = im2gray(im2double(imread('/Users/jamesdavid/Documents/Winter2022/EECS 351/project/matlabHandwriting/tempFolder/current.png')));
+            let = imresize(let,[50 50]);
+            let = let(:)';
+            label = predict(Mdl, let);
+            outputString = append(outputString,label);
+            if(prevRow - row > 250)
+                outputString = append(outputString," ");
+            end
+            prevRow = row;
+            firstLet = 0;
             1;
-
+            
         end
     end
     col = maxCol;
 end
+outputString = extractBetween(outputString,12,strlength(outputString));
 
+study_home = '/Users/jamesdavid/Documents/Winter2022/EECS 351/project/matlabHandwriting/pdf/';
+
+
+cd(study_home);
+
+
+SAVE_FIGURES = 2;
+OPEN_FIGURES = 0;
+
+this_latex_file_namepath = [study_home,'output.tex'];
+outputLatex = fopen(this_latex_file_namepath,'w');
+if outputLatex>2
+
+    fprintf(outputLatex, '\\documentclass[11pt]{article}\n');
+    fprintf(outputLatex, '\\usepackage[margin=0.5in]{geometry}\n');
+    fprintf(outputLatex, '\\geometry{letterpaper}\n');
+    fprintf(outputLatex, '%%\\geometry{showframe}\n');
+    fprintf(outputLatex, '\\usepackage[parfill]{parskip}    %% Activate to begin paragraphs with an empty line\n');
+    fprintf(outputLatex, '\\usepackage{graphicx}\n');
+    fprintf(outputLatex, '\\usepackage{xcolor}\n');
+    fprintf(outputLatex, '\\usepackage{amssymb}\n');
+    fprintf(outputLatex, '\\usepackage{float}\n');
+    fprintf(outputLatex, '\\usepackage{epstopdf}\n');
+    fprintf(outputLatex, '\\usepackage{hyperref}\n');
+    fprintf(outputLatex, '\\hypersetup{\n');
+    fprintf(outputLatex, '    colorlinks=true, %%set true if you want colored links\n');
+    fprintf(outputLatex, '    linktoc=all,     %%set to all if you want both sections and subsections linked\n');
+    fprintf(outputLatex, '    linkcolor=blue  %%choose some color if you want links to stand out\n');
+    fprintf(outputLatex, '}\n');
+    fprintf(outputLatex, '\n');
+    fprintf(outputLatex, '\\DeclareGraphicsRule{.tif}{png}{.png}{`convert #1 `dirname #1`/`basename #1 .tif`.png}\n');
+    fprintf(outputLatex, '\n');
+    fprintf(outputLatex, '\\title{Winter 2022 Final Project}\n');
+    fprintf(outputLatex, '\\author{Group 6}\n\n');
+    fprintf(outputLatex, '\\begin{document}\n');
+    fprintf(outputLatex, '\\maketitle\n');
+
+    fprintf(outputLatex, '\\tableofcontents');
+    fprintf(outputLatex, '\\newpage');
+
+    for scanNo=[1]% Chapters
+
+        fprintf(outputLatex, '\\newpage\n');
+        fprintf(outputLatex, '%% Chapter %d\n', scanNo);
+        fprintf(outputLatex, '\\section{Output from classifier}\n');
+        fprintf(outputLatex, '\\label{section%d}\n', scanNo);
+
+        fprintf(outputLatex, '\\subsection{Output}\n');
+        fprintf(outputLatex, '\\label{Sec%d_L2_Introduction}\n',scanNo);
+
+        fprintf(outputLatex, outputString);
+        
+        %
+        % trim command: left, bottom, right, top
+        %
+
+
+    end % of for-loop over all subjects
+
+    % %
+    %
+    % End Document
+    %
+    fprintf(outputLatex, '\\end{document}\n\n');
+
+    %
+    % now read the entire file and replace all underscores within quotation
+    % marks with '\_' to avoid the subscripts.  
+    %
+    fclose(outputLatex);
+    outputLatex = fopen(this_latex_file_namepath,'r');
+    allLaTeXText = fread(outputLatex, inf, '*char');
+    fclose(outputLatex);
+
+    numReplacements = 0;
+    Qopen = 1==0;
+    for qwe=1:numel(allLaTeXText)
+        if (allLaTeXText(qwe)=='$') && (allLaTeXText(qwe+1)=='"')
+            Qopen = ~Qopen;
+        elseif (allLaTeXText(qwe)=='"') && (allLaTeXText(qwe+1)=='$')
+            Qopen = ~Qopen;
+        elseif (allLaTeXText(qwe)=='_') && Qopen
+            numReplacements = numReplacements + 1;
+        end
+    end
+
+    newLaTeXText = char(zeros(numel(allLaTeXText)+numReplacements,1,'int8'));
+    Qopen = 1==0; asd=1;
+    for qwe=1:numel(allLaTeXText)
+        if (allLaTeXText(qwe)=='$') && (allLaTeXText(qwe+1)=='"')
+            Qopen = ~Qopen;
+        elseif (allLaTeXText(qwe)=='"') && (allLaTeXText(qwe+1)=='$')
+            Qopen = ~Qopen;
+        elseif (allLaTeXText(qwe)=='_') && Qopen
+            numReplacements = numReplacements + 1;
+            newLaTeXText(asd) = '\';
+            asd=asd+1;
+        end
+        newLaTeXText(asd) = allLaTeXText(qwe);
+        asd=asd+1;
+    end
+
+    outputLatex = fopen(this_latex_file_namepath,'w');
+    fwrite(outputLatex, newLaTeXText);
+    fclose(outputLatex);
+
+else
+    fprintf(2, 'Could not open LaTeX output file!\n');
+end % of if-open LaTeX file
